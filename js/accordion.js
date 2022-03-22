ef053f6c6d7341ec48d6efd6f382629c750648e2
curyPD <romandruzhinin2003@gmail.com>
2022-03-22 14:57:52 +0300

    Finished accordion component

--- js/accordion.js
+++ js/accordion.js
@@ -3,11 +3,18 @@ const accordion = document.querySelector(".accordion");
 accordion.addEventListener("click", function (e) {
   const btn = e.target.closest(".btn--icon");
   if (!btn) return;
+  const blockContent = btn.closest(".block").querySelector(".block__content");
 
   this.querySelectorAll(".block").forEach((block) => {
     if (block === btn.closest(".block")) return;
     block.classList.add("hidden");
+    block.querySelector(".block__content").style.maxHeight = null;
   });
 
   btn.closest(".block").classList.toggle("hidden");
+  if (blockContent.style.maxHeight) {
+    blockContent.style.maxHeight = null;
+  } else {
+    blockContent.style.maxHeight = blockContent.scrollHeight + "px";
+  }
 });
